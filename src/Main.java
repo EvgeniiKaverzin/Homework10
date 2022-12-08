768011a16f664dcde214ce03c6daff35665cd3ae
Евгений MSI <kaver_83@mail.ru>
2022-12-08 18:43:53 +0300

    Выполнил ДЗ 10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         //Задание 1.
         System.out.println("Задание 1");
         String firstName = "Ivan";
@@ -7,9 +8,11 @@ public class Main {
         String lastName = "Ivanov";
         String fullName  = lastName+" "+firstName+" "+middleName;
         System.out.println("ФИО сотрудника - " + fullName);
+
         //Задание 2.
         System.out.println("Задание 2");
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
         //Задание 3.
         System.out.println("Задание 3");
         fullName = "Иванов Семён Семёнович";
